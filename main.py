5049e9b4c655b2e9dc7dc1e351d06418f9041697
kaitlek <enterthepissjar@gmail.com>
2023-08-08 19:51:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from voicemessages import VoiceMessageSender
 
-TOKEN = "token"
-CHANNEL_ID = 123
+TOKEN = "your_token_here" # Replace with your token
+CHANNEL_ID = 123  # Replace with your channel ID
 
 voicemessage = VoiceMessageSender(TOKEN, CHANNEL_ID)
-voicemessage.SendVoiceMessage("D:\...")
+voicemessage.SendVoiceMessage("D:\path\to\your\audio\file.mp3")
